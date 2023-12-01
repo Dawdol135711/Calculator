4663f3a758ee2967d1a63bb941356c67a58ff7c6
Dawid <dawid130892@interia.pl>
2023-12-01 18:53:21 +0100

    first js

---
+++ script.js
@@ -0,0 +1,12 @@
+const viewer = document.querySelector(".viewer");
+const plus = document.querySelector(".plus");
+const minus = document.querySelector(".minus");
+const multiply = document.querySelector(".multiply");
+const divide = document.querySelector(".divide");
+const digits = document.querySelector(".digits")
+
+const showInsertion = function (button) {
+	viewer.innerHTML=(button)
+};
+showInsertion(12345);
+console.log(digits);
