2f0a9c6c8c31aa596d1657e5b52029bf548e5342
Dawid <dawid130892@interia.pl>
2023-12-04 20:46:12 +0100

    Added some js

--- script.js
+++ script.js
@@ -3,10 +3,19 @@ const plus = document.querySelector(".plus");
 const minus = document.querySelector(".minus");
 const multiply = document.querySelector(".multiply");
 const divide = document.querySelector(".divide");
-const digits = document.querySelector(".digits")
+const digits = document.querySelector(".digits");
+const digit = document.querySelectorAll(".digits li");
 
 const showInsertion = function (button) {
-	viewer.innerHTML=(button)
+	viewer.innerHTML = button;
 };
 showInsertion(12345);
-console.log(digits);
+console.log(digit);
+
+digit.forEach(function (number) {
+    const clickedNumber = function () {
+        viewer.innerHTML =number.innerText;
+    };
+
+    number.addEventListener('click', clickedNumber);
+});
